24c41dda40fda8793cdd379903a2444a0e314ec0
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-02-28 15:26:55 +0100

    .

--- math/geometry/convexhull.go
+++ math/geometry/convexhull.go
@@ -1,6 +1,7 @@
 package geometry
 
 import (
+	"math/bits"
 	"sort"
 
 	"github.com/lorenzotinfena/goji/math"
@@ -19,37 +20,37 @@ func orientation[E constraints.Signed | constraints.Float](p1, p2, p3 [2]E) E {
 	}
 }
 
-// Graham scan algorithm on 2-dim euclidean space E
-func GrahamScan[E constraints.Signed | constraints.Float](v [][2]E) [][2]E {
-	if len(v) == 0 {
+// Clockwise ordered Graham scan algorithm on 2-dim euclidean space E
+func GrahamScan[E constraints.Signed | constraints.Float](points [][2]E) [][2]E {
+	if len(points) == 0 {
 		return [][2]E{}
 	}
 
 	// Find starting point
-	start := v[0]
-	for i := 1; i < len(v); i++ {
-		if v[i][1] < start[1] || (v[i][1] == start[1] && v[i][0] < start[0]) {
-			start = v[i]
+	start := points[0]
+	for i := 1; i < len(points); i++ {
+		if points[i][1] < start[1] || (points[i][1] == start[1] && points[i][0] < start[0]) {
+			start = points[i]
 		}
 	}
 
 	// Remove duplicates of starting point (to avoid a bad sorting)
-	tmp := v[:0]
-	for i := 0; i < len(v); i++ {
-		if v[i] != start {
-			tmp = append(tmp, v[i])
+	v := [][2]E{start}
+	for i := 0; i < len(points); i++ {
+		if points[i] != start {
+			v = append(v, points[i])
 		}
 	}
-	v = tmp
 
 	// sort by orientation
-	sort.Slice(v, func(i, j int) bool {
-		return orientation(start, v[i], v[j]) > E(0)
+	tmp := v[1:]
+	sort.Slice(tmp, func(i, j int) bool {
+		return orientation(start, tmp[i], tmp[j]) > E(0)
 	})
 
 	// Keep only farthest point in colinear points
-	tmp = v[:0]
-	for i := 0; i < len(v); {
+	tmp = v[:1]
+	for i := 1; i < len(v); {
 		farthest := v[i]
 		i++
 		for i < len(v) && orientation(start, farthest, v[i]) == 0 {
@@ -62,12 +63,80 @@ func GrahamScan[E constraints.Signed | constraints.Float](v [][2]E) [][2]E {
 	}
 	v = tmp
 
-	stack := [][2]E{start}
-	for i := 0; i < len(v); i++ {
+	stack := v[:1]
+	for i := 1; i < len(v); i++ {
 		for len(stack) != 1 && orientation(stack[len(stack)-2], stack[len(stack)-1], v[i]) <= 0 {
 			stack = stack[:len(stack)-1]
 		}
 		stack = append(stack, v[i])
 	}
 	return stack
+}
+
+func ChansAlgorithm[E constraints.Signed | constraints.Float](v [][2]E) [][2]E {
+	maxT := (bits.LeadingZeros(0) - bits.LeadingZeros(uint(bits.LeadingZeros(0)-bits.LeadingZeros(uint(len(v)))-1)) - 1) + 1
+	for t := 1; t <= maxT; t++ {
+		var m int
+		if t == maxT {
+			m = len(v)
+		} else {
+			m = 1 << (1 << t)
+		}
+		convexhulls := make([][][2]E, (len(v)-1)/m+1)
+		for i := 0; i < len(convexhulls); i++ {
+			convexhulls[i] = GrahamScan(v[i*m : math.Min(len(v), (i+1)*m)])
+		}
+		kPointers := make([]int, len(convexhulls))
+		for k := 0; k < len(convexhulls); k++ {
+			c := convexhulls[k]
+			ministart := 0
+			for i := 1; i < len(c); i++ {
+				if c[i][1] < c[ministart][1] || (c[i][1] == c[ministart][1] && c[i][0] < c[ministart][0]) {
+					ministart = i
+				}
+			}
+			kPointers[k] = ministart
+		}
+		vertexSoFar := convexhulls[0][kPointers[0]]
+		for k := 1; k < len(kPointers); k++ {
+			if convexhulls[k][kPointers[k]][1] < vertexSoFar[1] || (convexhulls[k][kPointers[k]][1] == vertexSoFar[1] && convexhulls[k][kPointers[k]][0] == vertexSoFar[0]) {
+				vertexSoFar = v[kPointers[k]]
+			}
+		}
+		result := [][2]E{vertexSoFar}
+		for ; m >= 0; m-- {
+			// update kPointers without binary search (ti.inf.ethz.ch/ew/lehre/CG09/materials/v2.pdf)
+			for k := 0; k < len(kPointers); k++ {
+				if len(convexhulls[k]) == 1 {
+					continue
+				}
+				for {
+					current := convexhulls[k][kPointers[k]]
+					next := convexhulls[k][(kPointers[k]+1)%len(convexhulls[k])]
+					tmp := orientation(vertexSoFar, current, next)
+					if tmp > 0 && (tmp != 0 || (math.Abs(vertexSoFar[0]-next[0]) <= math.Abs(vertexSoFar[0]-current[0]) && math.Abs(vertexSoFar[1]-next[1]) <= math.Abs(vertexSoFar[1]-current[1]))) {
+						break
+					}
+
+					kPointers[k] = (kPointers[k] + 1) % len(convexhulls[k])
+				}
+			}
+
+			// Get next vertex of total convex hull (in case of colinear nexts, take the farthest)
+			next := convexhulls[0][kPointers[0]]
+			for k := 1; k < len(kPointers); k++ {
+				candidate := convexhulls[k][kPointers[k]]
+				tmp := orientation(vertexSoFar, candidate, next)
+				if tmp > 0 || (tmp == 0 && (math.Abs(vertexSoFar[0]-candidate[0]) > math.Abs(vertexSoFar[0]-next[0]) || math.Abs(vertexSoFar[1]-candidate[1]) > math.Abs(vertexSoFar[1]-next[1]))) {
+					next = candidate
+				}
+			}
+			if next == result[0] {
+				return result
+			}
+			result = append(result, next)
+			vertexSoFar = next
+		}
+	}
+	panic("error")
 }
--- math/geometry/convexhull_test.go
+++ math/geometry/convexhull_test.go
@@ -8,6 +8,36 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestGrahamScan2(t *testing.T) {
+	points := [][2]int{
+		{-2, 1},
+		{1, 1},
+		{1, 1},
+		{2, 3},
+	}
+	convexhull := geometry.GrahamScan(points)
+	sort.Slice(convexhull, func(i, j int) bool {
+		if convexhull[i][0] == convexhull[j][0] {
+			return convexhull[i][1] < convexhull[j][1]
+		} else {
+			return convexhull[i][0] < convexhull[j][0]
+		}
+	})
+	expectedConvexHull := [][2]int{
+		{-2, 1},
+		{2, 3},
+		{1, 1},
+	}
+	sort.Slice(expectedConvexHull, func(i, j int) bool {
+		if expectedConvexHull[i][0] == expectedConvexHull[j][0] {
+			return expectedConvexHull[i][1] < expectedConvexHull[j][1]
+		} else {
+			return expectedConvexHull[i][0] < expectedConvexHull[j][0]
+		}
+	})
+	assert.Equal(t, convexhull, expectedConvexHull)
+}
+
 func TestGrahamScan(t *testing.T) {
 	points := [][2]int{
 		{-2, 1},
@@ -29,6 +59,52 @@ func TestGrahamScan(t *testing.T) {
 		{0, -2},
 	}
 	convexhull := geometry.GrahamScan(points)
+	sort.Slice(convexhull, func(i, j int) bool {
+		if convexhull[i][0] == convexhull[j][0] {
+			return convexhull[i][1] < convexhull[j][1]
+		} else {
+			return convexhull[i][0] < convexhull[j][0]
+		}
+	})
+	expectedConvexHull := [][2]int{
+		{-3, -1},
+		{-1, 3},
+		{2, 3},
+		{1, -1},
+		{0, -3},
+		{-1, -3},
+	}
+	sort.Slice(expectedConvexHull, func(i, j int) bool {
+		if expectedConvexHull[i][0] == expectedConvexHull[j][0] {
+			return expectedConvexHull[i][1] < expectedConvexHull[j][1]
+		} else {
+			return expectedConvexHull[i][0] < expectedConvexHull[j][0]
+		}
+	})
+	assert.Equal(t, convexhull, expectedConvexHull)
+}
+
+func TestChansAlgorithm(t *testing.T) {
+	points := [][2]int{
+		{-2, 1},
+		{1, 1},
+		{-2, -1},
+		{1, 1},
+		{2, 3},
+		{-3, -1},
+		{-1, 1},
+		{0, 0},
+		{1, -1},
+		{-1, -2},
+		{-1, -3},
+		{-1, 3},
+		{-1, 1},
+		{-1, -3},
+		{0, -3},
+		{0, 0},
+		{0, -2},
+	}
+	convexhull := geometry.ChansAlgorithm(points)
 	sort.Slice(convexhull, func(i, j int) bool {
 		if convexhull[i][0] == convexhull[j][0] {
 			return convexhull[i][1] < convexhull[j][1]
