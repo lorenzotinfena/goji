ca3061b587674c814b17a54e632a8fec0a6e539e
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-02-29 23:29:21 +0100

    Update hash.go

--- math/hash.go
+++ math/hash.go
@@ -1,18 +1,17 @@
 package math
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/gob"
 	"hash/fnv"
-	"reflect"
 )
 
 // Non-cryptographic hash (FNV-1a)
 func Hash(data any) uint {
 	f := fnv.New64a()
-	if reflect.ValueOf(data).Kind() == reflect.Ptr {
-		f.Write([]byte(fmt.Sprintf("%p", interface{}(data))))
-	} else {
-		f.Write([]byte(fmt.Sprintf("%v", data)))
-	}
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	enc.Encode(data)
+	f.Write(buf.Bytes())
 	return uint(f.Sum64())
 }
