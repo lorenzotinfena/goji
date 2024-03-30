d10878ca229b298fe9c791fa382e7689ad0e0cf7
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-03-30 02:53:17 +0100

    .

--- collections/stack.go
+++ collections/stack.go
@@ -13,8 +13,13 @@ func NewStack[T any]() *Stack[T] {
 		l: *ll.NewSinglyLinkedList[T](nil),
 	}
 }
+
 func (s *Stack[T]) Len() int      { return s.l.Len() }
+
 func (s *Stack[T]) Push(value T)  { s.l.InsertFirst(value) }
+
 func (s *Stack[T]) Pop() T        { return s.l.RemoveFirst() }
+
 func (s *Stack[T]) Preview() T    { return s.l.First() }
+
 func (s Stack[T]) String() string { return s.l.String() }
