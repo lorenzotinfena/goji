9972f308dcf005a812f165c7489e3d741de508d8
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2024-04-17 22:36:13 +0200

    Update queue.go

--- collections/queue.go
+++ collections/queue.go
@@ -14,12 +14,12 @@ func NewQueue[T any]() *Queue[T] {
 	}
 }
 
-func (q *Queue[T]) Len() int        { return q.l.Len() }
+func (q *Queue[T]) Len() int { return q.l.Len() }
 
 func (q *Queue[T]) Enqueue(value T) { q.l.InsertLast(value) }
 
-func (q *Queue[T]) Dequeue() T      { return q.l.RemoveFirst() }
+func (q *Queue[T]) Dequeue() T { return q.l.RemoveFirst() }
 
-func (q *Queue[T]) Preview() T      { return q.l.First() }
+func (q *Queue[T]) Preview() T { return q.l.First() }
 
-func (q Queue[T]) String() string   { return q.l.String() }
+func (q Queue[T]) String() string { return q.l.String() }
